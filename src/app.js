04327336ef85fd2725651b3c141f09dcf6978622
Jamie Tater <jtater1@gmail.com>
2020-05-25 16:16:31 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+
+
+const app = express();
+
+const morganSetting = NODE_ENV === 'production' ? 'tiny' : 'dev';
+
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+})
+
+app.get('/', (req, req) => {
+    res.send("Hello, boilerplate!")
+})
+
+app.use((error, req, res, next) => {
+    let response
+    if(NODE_ENV === "production") {
+        response =  {error: {message: 'server error'}};
+    } else {
+        console.error(error);
+        response = {message: error.message, error};
+    }
+    res.status(500).json(response);
+})
+
+module.exports = app;
