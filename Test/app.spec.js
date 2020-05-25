3a172623335b8520d51d1cc7f3a3b7b5194506e8
Jamie Tater <jtater1@gmail.com>
2020-05-25 19:51:41 -0400

    added drills

--- Test/app.spec.js
+++
@@ -1,9 +0,0 @@
-const app = require('../src/app');
-
-describe('testing endpoint /', () => {
-    it('GET / responds with 200',()=>{
-        return supertest(app)
-            .get('/')
-            .expect(200)
-    })
-})
---
+++ src/drill.js
@@ -0,0 +1,59 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+})
+
+function searchForItem (searchTerm) {
+    knexInstance
+        .select('*')
+        .from('shopping_list')
+        .where('name', 'ILIKE', `%${searchTerm}%`)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+function allItemsPaginated (pageNumber){
+    const itemsPerPage = 6;
+    const offset = itemsPerPage * (pageNumber - 1);
+    knexInstance
+        .select('*')
+        .from('shopping_list')
+        .limit(itemsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+function itemsAddedAfterDate (daysAgo){
+    knexInstance
+        .select('*')
+        .from('shopping_list')
+        .where('date_added', 
+        '>',
+        knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+        )
+        .then(result => {
+            console.log(result)
+        })
+}
+
+function totalCostByCategory (){
+    knexInstance
+        .select('category')
+        .from('shopping_list')
+        .sum('price')
+        .groupBy('category')
+        .then(result => {
+            console.log(result)
+        })
+}
+
+searchForItem('burger');
+allItemsPaginated(2)
+itemsAddedAfterDate(3);
+totalCostByCategory();
